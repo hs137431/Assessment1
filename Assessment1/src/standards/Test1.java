74b68dbe6dac44710c3731f92ab7e39f1421a412
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-09-01 08:29:21 -0500

    First Assessment Complete

---
+++ Assessment1/src/standards/Test1.java
@@ -0,0 +1,12 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "says something funny");
+	}
+
+}
---
+++ Assessment1/src/standards/Test3.java
@@ -0,0 +1,14 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hello world");
+		//System.out.println("Hello world");
+		
+	}
+
+}
